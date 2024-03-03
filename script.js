4f07bc47ac9a8690cfff01b50495a790568565e9
Oliver Ardelean <148954618+Oliver-Ard@users.noreply.github.com>
2024-03-03 10:42:55 +0200

    Add feature to collect the names from the forms

--- script.js
+++ script.js
@@ -30,6 +30,187 @@ function Player(playerName, playerMarker) {
 	return { getPlayerName, getPlayerMark };
 }
 
+const createSections = (function () {
+	const welcomeSection = document.querySelector("#welcome-screen");
+	const selectPlayersSection = document.querySelector("#select-players-screen");
+	const gameSection = document.querySelector("#game-screen");
+	const endGameModal = document.querySelector("#end-game-modal");
+
+	function createWelcomeSection() {
+		// H1
+		const h1 = document.createElement("h1");
+		h1.textContent = "Welcome at";
+		const br = document.createElement("br");
+		h1.append(br);
+		h1TextNode = document.createTextNode("Tic-Tac-Toe");
+		h1.append(h1TextNode);
+		welcomeSection.append(h1);
+
+		// Button
+		const button = document.createElement("button");
+		button.setAttribute("id", "welcome-btn");
+		button.classList.add("standard-btn");
+		button.textContent = "Play Game";
+		welcomeSection.append(button);
+	}
+
+	function createSelectPlayersSection() {
+		// H2
+		const h2 = document.createElement("h2");
+		h2.setAttribute("id", "select-players-text");
+		h2.textContent = "Select Players";
+		selectPlayersSection.append(h2);
+
+		// Play Game Btn
+		const playGameBtn = document.createElement("button");
+		playGameBtn.setAttribute("id", "play-game-btn");
+		playGameBtn.classList.add("standard-btn", "play-game-btn");
+		playGameBtn.textContent = "Play";
+		selectPlayersSection.append(playGameBtn);
+
+		// Cards Wrapper
+		const cardsWrapper = document.createElement("div");
+		cardsWrapper.classList.add("cards-wrapper");
+
+		// Players Cards
+		const playerOneCard = createPlayerCard(
+			"Player One",
+			"X",
+			"Arthur",
+			"player-one-form",
+			"player-one-name"
+		);
+		cardsWrapper.append(playerOneCard);
+		const playerTwoCard = createPlayerCard(
+			"Player Two",
+			"O",
+			"Sadie",
+			"player-two-form",
+			"player-two-name"
+		);
+		cardsWrapper.append(playerTwoCard);
+		selectPlayersSection.append(cardsWrapper);
+	}
+
+	function createPlayerCard(
+		playerName,
+		playerSign,
+		placeholderText,
+		playerForm,
+		playerInputName
+	) {
+		// Player Card Container
+		const playerCard = document.createElement("div");
+		playerCard.classList.add("player-card");
+
+		// H3
+		const h3 = document.createElement("h3");
+		h3.textContent = playerName;
+		playerCard.append(h3);
+
+		// Player Sign
+		const playerSignText = document.createElement("p");
+		playerSignText.classList.add("player-sign");
+		playerSignText.textContent = playerSign;
+		playerCard.append(playerSignText);
+
+		// Player Form
+		const form = document.createElement("form");
+		form.setAttribute("id", playerForm);
+		form.setAttribute("action", "#");
+		form.setAttribute("method", "post");
+		// Form Label
+		const label = document.createElement("label");
+		label.setAttribute("for", playerInputName);
+		label.textContent = "Name";
+		form.append(label);
+		// Form Input
+		const input = document.createElement("input");
+		input.setAttribute("type", "text");
+		input.setAttribute("id", playerInputName);
+		input.setAttribute("name", "name");
+		input.setAttribute("placeholder", placeholderText);
+		input.setAttribute("required", "");
+		input.setAttribute("minlength", "4");
+		input.setAttribute("maxlength", "16");
+		form.append(input);
+		// Form Btn
+		const button = document.createElement("button");
+		button.classList.add("standard-btn");
+		button.textContent = "Select";
+		form.append(button);
+
+		playerCard.append(form);
+
+		return playerCard;
+	}
+
+	function createGameSection() {
+		// H2
+		const h2 = document.createElement("h2");
+		h2.setAttribute("id", "player-turn-text");
+		h2.textContent = "Sadie's Turn";
+		gameSection.append(h2);
+
+		// Game Board
+		const gameBoardContainer = document.createElement("div");
+		gameBoardContainer.setAttribute("id", "game-board");
+		gameBoardContainer.classList.add("game-board", "x");
+
+		// Game Board Cells
+		for (let i = 0; i <= 8; i++) {
+			const button = document.createElement("button");
+			button.classList.add("board-cell");
+			button.dataset.cell = `${i}`;
+			gameBoardContainer.append(button);
+		}
+		gameSection.append(gameBoardContainer);
+	}
+
+	function createEndGameModal() {
+		// H2
+		const h2 = document.createElement("h2");
+		h2.setAttribute("id", "winner-text");
+		h2.textContent = "Sadie Won!";
+		endGameModal.append(h2);
+
+		// Btns Wrapper
+		const btnsWrapper = document.createElement("div");
+		btnsWrapper.classList.add("btns-wrapper");
+
+		// Play Again Btn
+		const playAgainBtn = document.createElement("button");
+		playAgainBtn.setAttribute("id", "play-again-btn");
+		playAgainBtn.classList.add("standard-btn");
+		playAgainBtn.textContent = "Play Again";
+		btnsWrapper.append(playAgainBtn);
+
+		// Restart Game Btn
+		const restartGameBtn = document.createElement("button");
+		restartGameBtn.setAttribute("id", "restart-game-btn");
+		restartGameBtn.classList.add("standard-btn");
+		restartGameBtn.textContent = "Restart Game";
+		btnsWrapper.append(restartGameBtn);
+
+		endGameModal.append(btnsWrapper);
+	}
+	createWelcomeSection();
+	createSelectPlayersSection();
+	createGameSection();
+	createEndGameModal();
+
+	return {
+		welcomeSection,
+		selectPlayersSection,
+		gameSection,
+		endGameModal,
+		createWelcomeSection,
+		createSelectPlayersSection,
+		createGameSection,
+		createEndGameModal,
+	};
+})();
+
 function GameController(firstPlayer, secondPlayer) {
 	let board = gameBoard.getBoard();
 	const playerOne = firstPlayer;
@@ -63,12 +244,6 @@ function GameController(firstPlayer, secondPlayer) {
 
 	function switchPlayerTurn() {
 		currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
-
-		if (currentPlayer === playerOne) {
-			console.log(`Is ${playerOne.getPlayerName()}'s turn`);
-		} else {
-			console.log(`Is ${playerTwo.getPlayerName()}'s turn`);
-		}
 	}
 
 	function checkWinner() {
@@ -143,170 +318,74 @@ function GameController(firstPlayer, secondPlayer) {
 	};
 }
 
-const createSections = (function () {
-	const welcomeSection = document.querySelector("#welcome-screen");
-	const selectPlayersSection = document.querySelector("#select-players-screen");
-	const gameSection = document.querySelector("#game-screen");
-	const endGameModal = document.querySelector("#end-game-modal");
-
-	function createWelcomeSection() {
-		// H1
-		const h1 = document.createElement("h1");
-		h1.textContent = "Welcome at";
-		const br = document.createElement("br");
-		h1.append(br);
-		h1TextNode = document.createTextNode("Tic-Tac-Toe");
-		h1.append(h1TextNode);
-		welcomeSection.append(h1);
-
-		// Button
-		const button = document.createElement("button");
-		button.setAttribute("id", "welcome-btn");
-		button.classList.add("standard-btn");
-		button.textContent = "Play Game";
-		welcomeSection.append(button);
-	}
-
-	function createSelectPlayersSection() {
-		// H2
-		const h2 = document.createElement("h2");
-		h2.textContent = "Select Players";
-		selectPlayersSection.append(h2);
-
-		// Cards Wrapper
-		const cardsWrapper = document.createElement("div");
-		cardsWrapper.classList.add("cards-wrapper");
-
-		// Players Cards
-		const playerOneCard = createPlayerCard("Player One", "X", "Arthur");
-		cardsWrapper.append(playerOneCard);
-		const playerTwoCard = createPlayerCard("Player Two", "O", "Sadie");
-		cardsWrapper.append(playerTwoCard);
-
-		selectPlayersSection.append(cardsWrapper);
-	}
-
-	function createPlayerCard(playerName, playerSign, placeholderText) {
-		// Player Card Container
-		const playerCard = document.createElement("div");
-		playerCard.classList.add("player-card");
-
-		// H3
-		const h3 = document.createElement("h3");
-		h3.textContent = playerName;
-		playerCard.append(h3);
-
-		// Player Sign
-		const playerSignText = document.createElement("p");
-		playerSignText.classList.add("player-sign");
-		playerSignText.textContent = playerSign;
-		playerCard.append(playerSignText);
-
-		// Player Form
-		const form = document.createElement("form");
-		form.setAttribute("id", "player-form");
-		form.setAttribute("action", "#");
-		form.setAttribute("method", "post");
-		// Form Label
-		const label = document.createElement("label");
-		label.setAttribute("for", "name");
-		label.textContent = "Name";
-		form.append(label);
-		// Form Input
-		const input = document.createElement("input");
-		input.setAttribute("type", "text");
-		input.setAttribute("id", "name");
-		input.setAttribute("name", "name");
-		input.setAttribute("placeholder", placeholderText);
-		input.setAttribute("required", "");
-		input.setAttribute("minlength", "4");
-		input.setAttribute("maxlength", "16");
-		form.append(input);
-		// Form Btn
-		const button = document.createElement("button");
-		button.classList.add("standard-btn");
-		button.textContent = "Select";
-		form.append(button);
-
-		playerCard.append(form);
-
-		return playerCard;
-	}
-
-	function createGameSection() {
-		// H2
-		const h2 = document.createElement("h2");
-		h2.setAttribute("id", "player-turn-text");
-		h2.textContent = "Sadie's Turn";
-		gameSection.append(h2);
-
-		// Game Board
-		const gameBoardContainer = document.createElement("div");
-		gameBoardContainer.setAttribute("id", "game-board");
-		gameBoardContainer.classList.add("game-board", "x");
-
-		// Game Board Cells
-		for (let i = 0; i <= 8; i++) {
-			const button = document.createElement("button");
-			button.classList.add("board-cell");
-			button.dataset.cell = `${i}`;
-			gameBoardContainer.append(button);
-		}
-		gameSection.append(gameBoardContainer);
-	}
-
-	function createEndGameModal() {
-		// H2
-		const h2 = document.createElement("h2");
-		h2.setAttribute("id", "winner-text");
-		h2.textContent = "Sadie Won!";
-		endGameModal.append(h2);
-
-		// Btns Wrapper
-		const btnsWrapper = document.createElement("div");
-		btnsWrapper.classList.add("btns-wrapper");
-
-		// Play Again Btn
-		const playAgainBtn = document.createElement("button");
-		playAgainBtn.setAttribute("id", "play-again-btn");
-		playAgainBtn.classList.add("standard-btn");
-		playAgainBtn.textContent = "Play Again";
-		btnsWrapper.append(playAgainBtn);
-
-		// Restart Game Btn
-		const restartGameBtn = document.createElement("button");
-		restartGameBtn.setAttribute("id", "restart-game-btn");
-		restartGameBtn.classList.add("standard-btn");
-		restartGameBtn.textContent = "Restart Game";
-		btnsWrapper.append(restartGameBtn);
-
-		endGameModal.append(btnsWrapper);
-	}
-
-	// createGameSection();
-	// createEndGameModal();
-
-	return {
-		welcomeSection,
-		selectPlayersSection,
-		gameSection,
-		endGameModal,
-		createWelcomeSection,
-		createSelectPlayersSection,
-		createGameSection,
-		createEndGameModal,
-	};
-})();
-
 // Screen Controller
 const screenController = (function () {
+	// Welcome Section
+	const welcomeBtn = document.querySelector("#welcome-btn");
+	// Select Players Section
+	const playerOneForm = document.querySelector("#player-one-form");
+	const playerTwoForm = document.querySelector("#player-two-form");
+	const playerOneInput = document.querySelector("#player-one-name");
+	const playerTwoInput = document.querySelector("#player-two-name");
+	const playGameBtn = document.querySelector("#play-game-btn");
+	const selectPlayersText = document.querySelector("#select-players-text");
+	// Game Section
+	const playerTurnText = document.querySelector("#player-turn-text");
+	const gameBoardEl = document.querySelector("#game-board");
+	// End Game Section
+
+	let playerOneName = "";
+	let playerTwoName = "";
+	let game;
+
+	function playGame() {
+		if (playerOneName !== "" && playerTwoName !== "") {
+			game = GameController(
+				Player(playerOneName, "X"),
+				Player(playerTwoName, "O")
+			);
+			selectPlayersText.classList.remove("select-players-text");
+			switchSections(
+				createSections.selectPlayersSection,
+				createSections.gameSection
+			);
+
+			playerTurnText.textContent = `${game.getCurrentPlayerName()}'s Turn`;
+		} else {
+			selectPlayersText.classList.add("select-players-text");
+		}
+	}
+
 	// --Helper Functions--
 	function handleWelcomeBtn() {
-		createSections.createSelectPlayersSection();
 		switchSections(
 			createSections.welcomeSection,
 			createSections.selectPlayersSection
 		);
+	}
+
+	function handleFormInput(playerInput) {
+		if (playerInput.value !== "") {
+			playerInput.disabled = true;
+		}
+	}
+
+	function handlePlayerOneForm(e) {
+		e.preventDefault();
+		handleFormInput(playerOneInput);
+		playerOneName = playerOneInput.value;
+		playerTwoInput.focus();
+
+		console.log(playerOneName);
+	}
+
+	function handlePlayerTwoForm(e) {
+		e.preventDefault();
+		handleFormInput(playerTwoInput);
+		playerTwoName = playerTwoInput.value;
+		playGameBtn.focus();
+
+		console.log(playerTwoName);
 	}
 
 	function switchSections(elementOne, elementTwo) {
@@ -314,15 +393,11 @@ const screenController = (function () {
 		elementTwo.classList.toggle("hide");
 	}
 
-	function init() {
-		createSections.createWelcomeSection();
-		const welcomeBtn = document.querySelector("#welcome-btn");
-		welcomeBtn.addEventListener("click", handleWelcomeBtn);
-	}
 	// --Event Listeners--
-
-	document.addEventListener("DOMContentLoaded", init);
-
+	welcomeBtn.addEventListener("click", handleWelcomeBtn);
+	playerOneForm.addEventListener("submit", handlePlayerOneForm);
+	playerTwoForm.addEventListener("submit", handlePlayerTwoForm);
+	playGameBtn.addEventListener("click", playGame);
 	// document.addEventListener("click", () => {
 	// 	createSections.endGameModal.showModal();
 	// });
@@ -333,5 +408,3 @@ const screenController = (function () {
 	// 	}
 	// });
 })();
-
-const game = GameController(Player("Arthur", "X"), Player("Mary", "O"));
